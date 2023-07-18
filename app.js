dbd466e6dfccc02f70b1e3c02cd26a5391e0a3df
yegayv <yegay@sheridancollege.ca>
2023-07-18 19:41:14 -0400

    Created blog project. Added home, about and contacts pages. Created navigation bar. Styling added.

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+
+const connectDB = require("./server/config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Connect to Database
+connectDB();
+
+app.use(express.static("public"));
+
+//Templating Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
