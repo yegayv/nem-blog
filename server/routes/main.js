0599f05d8086a941015e97e294a14566d05eb606
yegayv <yegay@sheridancollege.ca>
2023-07-19 00:32:51 -0400

    Finished pagination. Added route for post id

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,7 +10,7 @@ router.get("", async (req, res) => {
       description: "Simple Blog created with NodeJS, Express & MongoDB.",
     };
 
-    let perPage = 10;
+    let perPage = 6;
     let page = req.query.page || 1;
 
     const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
@@ -29,6 +29,27 @@ router.get("", async (req, res) => {
       nextPage: hasNextPage ? nextPage : null,
     });
     res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    res.render("post", {
+      locals,
+      data,
+      currentRoute: `/post/${slug}`,
+    });
   } catch (error) {
     console.log(error);
   }
