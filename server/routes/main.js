1f46c234f9c62d32998805f460756bdf146f8045
yegayv <yegay@sheridancollege.ca>
2023-07-18 23:24:52 -0400

    Fixed post list dividers. Added page counter

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,13 +4,30 @@ const Post = require("../models/Post");
 
 //Routes
 router.get("", async (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description: "Simple Blog created with NodeJS, Express & MongoDB.",
-  };
-
   try {
-    const data = await Post.find();
+    const locals = {
+      title: "NodeJS Blog",
+      description: "Simple Blog created with NodeJS, Express & MongoDB.",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
     res.render("index", { locals, data });
   } catch (error) {
     console.log(error);
